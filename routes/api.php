2c6f477a88a6925fa9d7e0d2f3672df065f53925
Subir008 <subirkarmakar8aug@gmail.com>
2024-12-20 03:49:53 +0530

    Controller added, login .logout added

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CrudController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('signup' , [AuthController::class , 'signup']);
+Route::post('login' , [AuthController::class , 'login']);
+Route::get('login' , [AuthController::class , 'login'])->name('login');
+
+Route::middleware('auth:sanctum')->group(function(){
+    
+    Route::post('logout', [AuthController::class , 'logout']);
+
+    // 
+    Route::apiResource('crud', CrudController::class);
+});
